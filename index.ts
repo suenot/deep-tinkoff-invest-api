44a1b74ff3783b4dac77a8fe9bbcf628ba5a5bed
Eugen Soloviov <suenot@gmail.com>
2022-04-30 16:35:55 +0300

    new Types: Position, Figi, StringValue, NumberValue

--- index.ts
+++ index.ts
@@ -19,16 +19,16 @@ const deep = new DeepClient({ apolloClient, linkId: 206 });
 const main = async () => {
   const actualUserId = deep.linkId; // actualUserId
   console.log('actualUserId', actualUserId);
-  
+
   const Type = await deep.id('@deep-foundation/core', 'Type') // Type type link id
   console.log('Type', Type);
-  
+
   const Package = await deep.id('@deep-foundation/core', 'Package') // Package type link Id
   console.log('Package', Package);
-  
+
   const Contain = await deep.id('@deep-foundation/core', 'Contain') // Contain type link id
   console.log('Contain', Contain);
-  
+
   const Any = await deep.id('@deep-foundation/core', 'Any');
   console.log('Any', Any);
 
@@ -37,6 +37,83 @@ const main = async () => {
 
   const String = await deep.id('@deep-foundation/core', 'String'); // String link id - use it as symbol
   console.log('String', String);
+
+  const Number = await deep.id('@deep-foundation/core', 'Number'); // Number link id - use it as symbol
+  console.log('Number', Number);
+
+  const { data: [{ id: packageId }] } = await deep.insert({
+    type_id: Package,
+    string: { data: { value: `@deep-foundation/tinkoff-invest-api` } },
+    in: { data: {
+      type_id: Contain,
+      from_id: actualUserId
+    } },
+  });
+  console.log('packageId', packageId);
+
+  // insert type StringValue
+  const { data: [{ id: StringValue }] } = await deep.insert({
+    // StringValue just dot
+    type_id: Type,
+    in: { data: {
+      // nest into package as 'StringValue' in Contain's tree under package
+      type_id: Contain,
+      from_id: packageId, // before created package
+      string: { data: { value: 'StringValue' } },
+    } },
+    out: { data: {
+      // StringValue can have .string as .value of link.
+      type_id: Value,
+      to_id: String,
+    } },
+  });
+  console.log('StringValue', StringValue);
+
+  // insert type NumberValue
+  const { data: [{ id: NumberValue }] } = await deep.insert({
+    // NumberValue just dot
+    type_id: Type,
+    in: { data: {
+      // nest into package as 'NumberValue' in Contain's tree under package
+      type_id: Contain,
+      from_id: packageId, // before created package
+      string: { data: { value: 'NumberValue' } },
+    } },
+    out: { data: {
+      // NumberValue can have .number as .value of link.
+      type_id: Value,
+      to_id: Number,
+    } },
+  });
+  console.log('NumberValue', NumberValue);
+
+  // insert type Position
+  const { data: [{ id: Position }] } = await deep.insert({
+    // Position just dot
+    type_id: Type,
+    in: { data: {
+      // nest into package as 'Position' in Contain's tree under package
+      type_id: Contain,
+      from_id: packageId, // before created package
+      string: { data: { value: 'Position' } },
+    } },
+  });
+  console.log('Position', Position);
+
+  // insert type Figi
+  const { data: [{ id: Figi }] } = await deep.insert({
+    // Figi just dot
+    from_id: Position,
+    to_id: StringValue,
+    type_id: Type,
+    in: { data: {
+      // nest into package as 'Figi' in Contain's tree under package
+      type_id: Contain,
+      from_id: packageId, // before created package
+      string: { data: { value: 'Figi' } },
+    } },
+  });
+  console.log('Figi', Figi);
 }
 
 main()
