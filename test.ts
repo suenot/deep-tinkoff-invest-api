4db3651d6b8ca12d8d1b5785196a3ac2432a4f33
Eugen Soloviov <suenot@gmail.com>
2022-05-21 20:02:13 +0300

    packages, tslint, mocha

--- test.ts
+++ test.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import 'mocha';
 import { expect } from 'chai';
 import _ from 'lodash';
@@ -8,6 +9,7 @@ const debug = require('debug')('bot').extend('balancer');
 describe('bot', () => {
   describe('balancer', () => {
     it.only('#1', async () => {
+      debug('#1');
       expect(undefined).to.equal(undefined);
     })
   })
