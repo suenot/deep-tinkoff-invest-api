5d19e60ecbbb3322a3467ae38ae6c1ed24784219
Eugen Soloviov <suenot@gmail.com>
2022-05-21 19:47:00 +0300

    test.ts не запускается

---
+++ test.ts
@@ -0,0 +1,14 @@
+import 'mocha';
+import { expect } from 'chai';
+import _ from 'lodash';
+// import uniqid from 'uniqid';
+
+const debug = require('debug')('bot').extend('balancer');
+
+describe('bot', () => {
+  describe('balancer', () => {
+    it.only('#1', async () => {
+      expect(undefined).to.equal(undefined);
+    })
+  })
+})
