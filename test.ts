cc20405ec1c48b5fbe449b0627ab745d282d2d89
Eugen Soloviov <suenot@gmail.com>
2022-05-22 15:05:11 +0300

    start first tests

--- test.ts
+++ test.ts
@@ -6,10 +6,57 @@ import _ from 'lodash';
 
 const debug = require('debug')('bot').extend('balancer');
 
+const sumValues = obj => Object.values(obj).reduce((a: number, b: number) => a + b);
+
+interface Wallet {
+  [key: string]: Number;
+}
+
+const normalizeDesire = (wallet: Wallet): Wallet => {
+  debug('Нормализуем проценты, чтобы общая сумма была равна 100%, чтобы исключить человеческий фактор');
+  debug('wallet', wallet);
+
+  const walletSum: number = Number(sumValues(wallet));
+  debug('walletSum', walletSum);
+
+  const normalizedDesire = Object.entries(wallet).reduce((p, [k, v]) => ({ ...p, [k]: (Number(v) / walletSum * 100) }), {});
+  debug('normalizedDesire', normalizedDesire);
+
+  return normalizedDesire;
+};
+
 describe('bot', () => {
   describe('balancer', () => {
-    it.only('#1', async () => {
-      debug('#1');
+          // const wallet = {
+      //   AAPL: 2,
+      //   USD: 1000,
+      // };
+    it.only('Test normalizeDezire', async () => {
+      const desiredWallet = {
+        AAPL: 100,
+        USD: 50,
+      };
+      const normalizedDesire = normalizeDesire(desiredWallet);
+
+      expect(normalizedDesire).to.deep.equal({ AAPL: 66.66666666666666, USD: 33.33333333333333 });
+    });
+
+    it.skip('#1', async () => {
+      // Нужно узнать лотность и последнюю ценю
+      // Использовать данные последних цен
+      // Лотность берется из инструмента
+      const wallet = {
+        AAPL: 2,
+        USD: 1000,
+      };
+      const desiredWallet = {
+        AAPL: 100,
+        USD: 50,
+      };
+      const normalizedDesire = normalizeDesire(desiredWallet);
+
+
+
       expect(undefined).to.equal(undefined);
     })
   })
